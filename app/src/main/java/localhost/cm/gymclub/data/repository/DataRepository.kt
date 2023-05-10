0b857cd79b7e25782c1ad6395c51ad1abfa5af42
André Sousa <andrematosdesousa@gmail.com>
2023-05-10 17:55:44 +0100

    Added the option to delete a plan

--- app/src/main/java/localhost/cm/gymclub/data/repository/DataRepository.kt
+++ app/src/main/java/localhost/cm/gymclub/data/repository/DataRepository.kt
@@ -5,6 +5,10 @@ import localhost.cm.gymclub.data.service.DataService
 class DataRepository(private val dataService: DataService) {
     suspend fun getPlans() = dataService.getPlans()
 
+    suspend fun getPlan(planId: Int) = dataService.getPlan(planId)
+
+    suspend fun deletePlan(planId: Int) = dataService.deletePlan(planId)
+
     suspend fun getWorkoutsForPlan(planId: Int) = dataService.getWorkoutForPlan(planId)
 
     suspend fun getGyms() = dataService.getGyms()
--- app/src/main/java/localhost/cm/gymclub/ui/workouts/PlanWorkoutsViewModel.kt
+++ app/src/main/java/localhost/cm/gymclub/ui/workouts/PlanWorkoutsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import localhost.cm.gymclub.data.entity.response.TrainingPlanResponse
 import localhost.cm.gymclub.data.entity.response.WorkoutResponse
 import localhost.cm.gymclub.data.repository.DataRepository
 import javax.inject.Inject
@@ -12,10 +13,18 @@ import javax.inject.Inject
 @HiltViewModel
 class PlanWorkoutsViewModel @Inject constructor(private val dataRepository: DataRepository) : ViewModel() {
     val workouts = MutableLiveData<List<WorkoutResponse>>()
+    val plan = MutableLiveData<TrainingPlanResponse>()
 
-    fun getWorkoutsForPlanId(planId: Int) {
+    fun getWorkoutsAndPlanByPlanId(planId: Int) {
         viewModelScope.launch {
+            plan.postValue(dataRepository.getPlan(planId))
             workouts.postValue(dataRepository.getWorkoutsForPlan(planId))
+        }
+    }
+
+    fun deletePlanByPlanId(planId: Int) {
+        viewModelScope.launch {
+            dataRepository.deletePlan(planId)
         }
     }
 }
