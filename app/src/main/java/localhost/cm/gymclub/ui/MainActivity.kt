6c075f0b7fae069fcacda00c7e028fc5f6ed201c
André Sousa <andrematosdesousa@gmail.com>
2023-05-10 19:34:41 +0100

    Added actionbar back button to the application

--- app/src/main/java/localhost/cm/gymclub/ui/MainActivity.kt
+++ app/src/main/java/localhost/cm/gymclub/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package localhost.cm.gymclub.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,17 +16,25 @@ import localhost.cm.gymclub.ui.auth.login.LoginActivity
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // bottom navigation setup
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        val navController = navHostFragment?.findNavController()
+        navController = navHostFragment?.findNavController()
         navController?.let {
             findViewById<BottomNavigationView>(R.id.nav_view).setupWithNavController(
                 it
             )
+
+            setupActionBarWithNavController(it)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController?.navigateUp() ?: super.onSupportNavigateUp()
     }
 }
